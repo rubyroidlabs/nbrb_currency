87beebc51005e9068876ef01db0934b416f07c05
Dmitry Artyomov <artyomovd@yandex.ru>
2019-01-24 23:24:36 +0300

    1.1.0 version: update to latest Money version, refactor tests and update test data

--- nbrb_currency.gemspec
+++ nbrb_currency.gemspec
@@ -3,10 +3,10 @@ $:.push File.expand_path("../lib", __FILE__)
 
 Gem::Specification.new do |s|
   s.name        = "nbrb_currency"
-  s.version     = "1.0.1"
+  s.version     = "1.1.0"
   s.platform    = Gem::Platform::RUBY
-  s.authors     = ["Aleks Grebennik"]
-  s.email       = ["aleks.grebennik@gmail.com"]
+  s.authors     = ["Aleks Grebennik", "Dmitry Artyomov"]
+  s.email       = ["aleks.grebennik@gmail.com", "artyomovd@yandex.ru"]
   s.homepage    = "http://github.com/slbug/nbrb_currency"
   s.summary     = %q{Calculates exchange rates based on rates from National Bank of the Republic of Belarus. Money gem compatible.}
   s.description = %q{This gem reads exchange rates from the National Bank of the Republic of Belarus website. It uses it to calculates exchange rates. It is compatible with the money gem}
@@ -14,11 +14,9 @@ Gem::Specification.new do |s|
   s.required_rubygems_version = ">= 1.3.6"
 
   s.add_dependency "nokogiri"
-  s.add_dependency "money",    "~> 6.5.0"
+  s.add_dependency "money",    "~> 6.13"
 
   s.add_development_dependency "rspec", ">= 2.0.0"
-  s.add_development_dependency "rr"
-  s.add_development_dependency "shoulda"
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
--- spec/nbrb_currency_spec.rb
+++ spec/nbrb_currency_spec.rb
@@ -1,79 +1,80 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'yaml'
 
-describe "NbrbCurrency" do
-  before(:each) do
-    @bank = NbrbCurrency.new
-    @cache_path = File.expand_path(File.dirname(__FILE__) + '/exchange_rates.xml')
-    @yml_cache_path = File.expand_path(File.dirname(__FILE__) + '/exchange_rates.yml')
-    @tmp_cache_path = File.expand_path(File.dirname(__FILE__) + '/tmp/exchange_rates.xml')
-    @exchange_rates = YAML.load_file(@yml_cache_path)
+describe 'NbrbCurrency' do
+  def test_rate(currency, amount = default_amount)
+    exchange_rates['currencies'][currency].to_f * 100 * amount
   end
+
+  let(:bank) { NbrbCurrency.new }
+  let(:cache_path) { File.expand_path(File.dirname(__FILE__) + '/exchange_rates.xml') }
+  let(:yml_cache_path) { File.expand_path(File.dirname(__FILE__) + '/exchange_rates.yml') }
+  let(:tmp_cache_path) { File.expand_path(File.dirname(__FILE__) + '/tmp/exchange_rates.xml') }
+  let(:exchange_rates) { YAML.load_file(yml_cache_path) }
+  let(:default_amount) { 100 }
 
   after(:each) do
-    if File.exists? @tmp_cache_path
-      File.delete @tmp_cache_path
-    end
+    File.delete(tmp_cache_path) if File.exist?(tmp_cache_path)
   end
 
-  it "should save the xml file from nbrb given a file path" do
-    @bank.save_rates(@tmp_cache_path)
-    File.exists?(@tmp_cache_path).should == true
+  it 'should save the xml file from nbrb given a file path' do
+    bank.save_rates(tmp_cache_path)
+    expect(File.exist?(tmp_cache_path)).to be true
   end
 
-  it "should raise an error if an invalid path is given to save_rates" do
-    lambda { @bank.save_rates(nil) }.should raise_exception
+  it 'should raise an error if an invalid path is given to save_rates' do
+    expect { bank.save_rates(nil) }.to raise_error(InvalidCache)
   end
 
-  it "should update itself with exchange rates from nbrb website" do
-    stub(OpenURI::OpenRead).open(NbrbCurrency::NBRB_RATES_URL) {@cache_path}
-    @bank.update_rates
+  it 'should update itself with exchange rates from nbrb website' do
+    allow(OpenURI::OpenRead).to receive(:open).with(NbrbCurrency::NBRB_RATES_URL) { cache_path }
+    bank.update_rates
     NbrbCurrency::CURRENCIES.each do |currency|
-      @bank.get_rate(currency, "BYR").should > 0
+      expect(bank.get_rate(currency, 'BYN')).to be > 0
+      expect(bank.get_rate(currency, 'BYR')).to be > 0
     end
   end
 
-  it "should update itself with exchange rates from cache" do
-    @bank.update_rates(@cache_path)
+  it 'should update itself with exchange rates from cache' do
+    bank.update_rates(cache_path)
     NbrbCurrency::CURRENCIES.each do |currency|
-      @bank.get_rate(currency, "BYR").should > 0
+      expect(bank.get_rate(currency, 'BYN')).to be > 0
+      expect(bank.get_rate(currency, 'BYR')).to be > 0
     end
   end
 
-  it "should return the correct exchange rates using exchange" do
-    @bank.update_rates(@cache_path)
-    NbrbCurrency::CURRENCIES.reject{|c| %w{JPY KWD}.include?(c) }.each do |currency|
-      @bank.exchange(100, currency, "BYR").cents.should == (@exchange_rates["currencies"][currency].to_f * 100).round
+  it 'should return the correct exchange rates using exchange' do
+    bank.update_rates(cache_path)
+    NbrbCurrency::CURRENCIES.each do |currency|
+      subunit = Money::Currency.wrap(currency).subunit_to_unit.to_f
+
+      expected_byn = test_rate(currency).round
+      expect(bank.exchange(default_amount * subunit, currency, 'BYN').cents).to eq(expected_byn)
+
+      expected_byr = (test_rate(currency) * NbrbCurrency::DENOMINATION_RATE / 100).round
+      expect(bank.exchange(default_amount * subunit, currency, 'BYR').cents).to eq(expected_byr)
     end
-    subunit = Money::Currency.wrap("KWD").subunit_to_unit.to_f
-    @bank.exchange(1000, "KWD", "BYR").cents.should == ((subunit / 1000) * @exchange_rates["currencies"]['KWD'].to_f * 100).round
-    subunit = Money::Currency.wrap("JPY").subunit_to_unit.to_f
-    @bank.exchange(100, "JPY", "BYR").cents.should == ((subunit * 100) * @exchange_rates["currencies"]['JPY'].to_f * 100).round
   end
 
-  it "should return the correct exchange rates using exchange_with" do
-    @bank.update_rates(@cache_path)
-    NbrbCurrency::CURRENCIES.reject{|c| %w{JPY KWD}.include?(c) }.each do |currency|
-      @bank.exchange_with(Money.new(100, currency), "BYR").cents.should == (@exchange_rates["currencies"][currency].to_f * 100).round
-      @bank.exchange_with(1.to_money(currency), "BYR").cents.should == (@exchange_rates["currencies"][currency].to_f * 100).round
+  it 'should return the correct exchange rates using exchange_with' do
+    bank.update_rates(cache_path)
+    NbrbCurrency::CURRENCIES.each do |currency|
+      subunit = Money::Currency.wrap(currency).subunit_to_unit.to_f
+
+      expected_byn = test_rate(currency).round
+      expect(bank.exchange_with(Money.new(default_amount * subunit, currency), 'BYN').cents).to eq(expected_byn)
+
+      expected_byr = (test_rate(currency) * NbrbCurrency::DENOMINATION_RATE / 100).round
+      expect(bank.exchange_with(Money.new(default_amount * subunit, currency), 'BYR').cents).to eq(expected_byr)
     end
-    @bank.exchange_with(5000.to_money('JPY'), 'BYR').cents.should == 55971500 # 559715 BYR
   end
 
   # in response to #4
-  it "should exchange btc" do
-    Money::Currency::TABLE[:btc] = {
-      :priority        => 1,
-      :iso_code        => "BTC",
-      :name            => "Bitcoin",
-      :symbol          => "BTC",
-      :subunit         => "Cent",
-      :subunit_to_unit => 1000,
-      :separator       => ".",
-      :delimiter       => ","
-    }
-    @bank.add_rate("USD", "BTC", 1 / 13.7603)
-    @bank.add_rate("BTC", "USD", 13.7603)
-    @bank.exchange(100, "BTC", "USD").cents.should == 138
+  it 'should exchange btc' do
+    bank.add_rate('USD', 'BTC', 1 / 13.7603)
+    bank.add_rate('BTC', 'USD', 13.7603)
+
+    subunit = Money::Currency.wrap('BTC').subunit_to_unit.to_f
+    expect(bank.exchange(10 * subunit, 'BTC', 'USD').to_f).to eq(137.6)
   end
 end
